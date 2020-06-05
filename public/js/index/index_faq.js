e42677315b5297f92f83bdf3b28431eb993ec82c
arshita04 <arshi.narayan04@gmail.com>
2020-06-05 19:34:50 +0530

    changing faq and features

--- public/js/index/index_faq.js
+++ public/js/index/index_faq.js
@@ -8,7 +8,18 @@ if (content.style.maxHeight){
  this.style.transform="rotate(0deg)";
 content.style.maxHeight = null;
 } else {
- this.style.transform="rotate(180deg)";
+  var collapsed = document.getElementsByClassName("faq-collapse");
+  var buttons = document.getElementsByClassName("faq-btn");
+  var j;
+  for(j=0;j<collapsed.length;j++)
+    {
+        if(collapsed[j].style.maxHeight)
+        {
+            buttons[j].style.transform="rotate(0deg)";
+            collapsed[j].style.maxHeight = null;
+        }
+    }
+this.style.transform="rotate(180deg)";
 content.style.maxHeight = content.scrollHeight + "px";
 } 
 });
