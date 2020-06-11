55e7f251ea0d21e49db9172e72595f610d2c6a7a
SUBHRASANKHA <imsubhranasaastronaut@gmail.com>
2020-06-11 16:51:25 +0530

    new

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,7 +1,62 @@
 const express = require("express");
 const router = express.Router();
 const Blog = require("../models/blog");
+const authorization = require("../middleware/auth");
 const multer = require('multer');
+const uuid = require("uuid");
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
 
+router.use(bodyParser.json());
+
+router.get('/', function(req, res){
+    res.render('blog');
+  });
+//Establish Storage for file upload 
+const storage = multer.diskStorage({
+	destination: function(req,file,cb){
+		// console.log(req.body);
+		const newDestination = __dirname+`/../../public/upload/cover/${req.body.email}`;
+		console.log("New Destination: ", newDestination);
+		var stat = null;
+		try{
+			stat = fs.statSync(newDestination);
+		}
+		catch(err){
+			fs.mkdir(newDestination,{recursive:true},(err)=>{
+				if(err)
+					console.error('New Directory Error: ',err);
+				else
+					console.log('New Directory Success');
+			})
+		}
+		if(stat && !stat.isDirectory())
+			throw new Error('Directory Couldnt be created');
+		cb(null,newDestination);
+	},
+	filename:function(req,file,cb){
+		cb(null,file.fieldname + '-' + uuid.v4() + path.extname(file.originalname));
+	}
+});
+
+router.post('/create',authorization,(req,res)=>{
+    const {body,pic} = req.body 
+    if(!body || !pic){
+      return  res.status(422).json({error:"Plase add all the fields"})
+    }
+    req.user.password = undefined
+    const post = new Post({
+        body,
+        photo:pic,
+        postedBy:req.user
+    })
+    post.save().then(result=>{
+        res.json({post:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
 module.exports=router;
