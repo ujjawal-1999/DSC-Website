0f9c8539dbbdb460c78eb6992a83605c356c72a5
Utkarsh Tiwari <utiwari914@gmail.com>
2020-06-11 23:07:06 +0530

    update blog schema, blog routes, and use html string to store blog body

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -2,16 +2,12 @@ let express = require('express');
 let router	= express.Router();
 const Blog = require('../models/blog')
 var slugify = require('slugify')
-var QuillDeltaToHtmlConverter = require('quill-delta-to-html').QuillDeltaToHtmlConverter;
+
+
 
 router.get('/', (req, res)=>{
-    res.render("blogs")
+	res.render("blogs");
 })
-
-var config = {
-	//include inline styling in the resulting html from delta
-	inlineStyles: false
-};
 
 
 
@@ -27,28 +23,24 @@ router.post('/create', async (req, res)=>{
 
 	try {
 		const blog = req.body;
-		if (!blog) return res.status(400).json({error: "empty query sent"})
+		if (!blog) return res.status(400).json({ error: "empty query sent" });
 
 		await new Blog({
 			title: blog.title,
 			author: blog.author,
-			body: JSON.parse(blog.body),
+			body: blog.body.toString(),
 			category: blog.category,
-			slug: (slugify(blog.title) + '-' + Math.random().toString(36).substr(2, 6)).toLowerCase()
+			slug: (slugify(blog.title) + '-' + Math.random().toString(36).substr(2, 8)).toLowerCase(),
+			summary: blog.summary
 		})
 		.save((err, saved)=> {
-			if (err) {
-				console.log(err)
-				res.status(400).json({error: "Some error occured"});
-				return err;	
-			}
 			res.json(saved);
 		})
 	}
 
 	catch(e) {
 		console.log(e)
-		res.status(400).json({error: "Some error occured"})
+		res.status(400).json({error: "Some error occured"});
 		return e;
 	}
 })
@@ -60,20 +52,16 @@ router.get('/view/:slug', async (req, res)=>{
 
 	try{
 		//find the corresponding blog in db
-		let slug = req.params.slug
-		if (!slug) return res.status(400).json({error: "empty query sent"})
+		let slug = req.params.slug;
+		if (!slug) return res.status(400).json({error: "empty query sent"});
 
-		const blog = await Blog.findOne({slug});
-		var deltaOps =	blog.body.ops;
-		var converter = new QuillDeltaToHtmlConverter(deltaOps, config);
-		var html = converter.convert();
-
+		const blog = await Blog.findOne({ slug });
 		//render result page with resulting html
-		res.render('show-blog', { blogContent: html })
+		res.render('show-blog', { blogContent: blog.body });
 	}
 
 	catch(e) {
-		res.status(400).json({error: "some error occured"})
+		res.status(400).json({error: "some error occured"});
 		return e;	
 	}
 })
