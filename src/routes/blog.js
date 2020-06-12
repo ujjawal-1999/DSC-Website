525545c59b09625b498a75dd2001388f8f5ecef6
anuva04 <57056078+anuva04@users.noreply.github.com>
2020-06-13 00:35:22 +0530

    mobile view of filter and data-aos

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -10,8 +10,9 @@ router.get('/', async(req, res)=>{
 	res.render("blogs",{user:req.user,found:finduser});
 })
 
-router.get('/fullblog', (req, res)=>{
-	res.render("fullblog");
+router.get('/fullblog', async(req, res)=>{
+	const finduser = await User.find();
+	res.render("fullblog",{user:req.user,found:finduser});
 })
 
 
